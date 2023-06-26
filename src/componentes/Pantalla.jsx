360fa931beaf4d9149c80a60d6ff142ae35597b7
NachoM57 <ignaciomaiorino50@gmail.com>
2023-06-26 13:20:13 -0300

    calculadora terminada con react

---
+++ src/componentes/Pantalla.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style.css/Pantalla.css";
+
+
+
+const Pantalla = ({ input }) => (
+    <div className="input">
+        {input}
+    </div>
+);
+
+
+export default Pantalla;
+
+
+
